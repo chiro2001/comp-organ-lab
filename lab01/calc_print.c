88c81ee8ce499a87f735dbb3ea37614834ae19c2
Chiro Liang <chiro2001@163.com>
2022-04-21 10:52:10 +0800

    add %_print

---
+++ lab01/calc_print.c
@@ -0,0 +1,21 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+  uint8_t code = 19;
+  uint8_t i = 0;
+  uint32_t sum = 0;
+  while (i != 8) {
+    if (code & (1 << i)) sum += code << i;
+    i++;
+  }
+  uint32_t sq = sum;
+  i = 0;
+  sum = 0;
+  while (i != 16) {
+    if (code & (1 << i)) sum += sq << i;
+    i++;
+  }
+  printf("%d\n", sum);
+  return 0;
+}
