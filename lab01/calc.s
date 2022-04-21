fa3958228da981bb9560c2792370ea2f6cea25a3
Chiro Liang <chiro2001@163.com>
2022-04-21 10:58:05 +0800

    add cflags

--- lab01/calc.s
+++ lab01/calc.s
@@ -1,10 +1,10 @@
 	.file	"calc.c"
 	.option nopic
-	.attribute arch, "rv64i2p0_m2p0_a2p0_f2p0_d2p0_c2p0"
+	.attribute arch, "rv64i2p0"
 	.attribute unaligned_access, 0
 	.attribute stack_align, 16
 	.text
-	.align	1
+	.align	2
 	.globl	_start
 	.type	_start, @function
 _start:
